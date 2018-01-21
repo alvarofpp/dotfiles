4f1ff523de7efb9e0ebaddff89ad0de774f9c461
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-01-21 18:31:03 -0300

    Adjustments

--- program.sh
+++ program.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# ---------
-# FUNCTIONS
-# ---------
+# -------------
+# | FUNCTIONS |
+# -------------
 # Remove warning of the Gtk-Message
 function zenity() {
     /usr/bin/zenity "$@" 2>/dev/null
@@ -14,54 +14,64 @@ function Main() {
     --column "pick" --column "options" \
     TRUE "Fix bugs" \
     FALSE "Programs" \
-    FALSE "Languages" \
+    FALSE "Programming languages" \
   );
 
   # Checks if user clicked "Cancel"
-  if [ $? -eq 1 ];
-  then
+  if [ $? -eq 1 ]; then
     exit 1
   fi
 
   # Redirects to the desired option
-  if [ "$way" == "Fix bugs" ];
-  then
+  if [ "$way" == "Fix bugs" ]; then
     Bugs
-  elif [ "$way" == "Programs" ];
-  then
+  elif [ "$way" == "Programs" ]; then
     Programs
   else
     Languages
   fi
 }
 
+
+# ----
+# Bugs
+# ----
 function Bugs() {
   options=$( \
     zenity --list --title "Fix bugs" --checklist \
-    --column "pick" --column "options" \
+    --column "select" --column "options" \
     TRUE "Double-click with touchpad" \
     FALSE "Input of sound not recognize" \
     --separator=":"\
   );
 }
 
+
+# --------
+# Programs
+# --------
 function Programs() {
   options=$( \
     zenity --list --title "Programs" --text "Select the programs you prefer" --checklist \
-    --column "pick" --column "options" \
-    FALSE "Git" \
-    FALSE "PHP" \
-    FALSE "Python" \
-    FALSE "R" \
+    --column "select" --column "options" \
+    TRUE "Git" \
+    TRUE "Spotify" \
+    FALSE "PHPStorm" \
+    FALSE "DataGrip" \
+    FALSE "Anaconda" \
+    FALSE "PGAdmin" \
     --separator=":"\
   );
 }
 
+
+# ---------
+# Languages
+# ---------
 function Languages() {
   options=$( \
-    zenity --list --title "Languages" --text "Select the languages you prefer" --checklist \
-    --column "pick" --column "options" \
-    FALSE "Git" \
+    zenity --list --title "Programming languages" --text "Select the languages you prefer" --checklist \
+    --column "select" --column "options" \
     FALSE "PHP" \
     FALSE "Python" \
     FALSE "R" \
@@ -69,8 +79,9 @@ function Languages() {
   );
 }
 
-# ---------
-# CODE
-# ---------
+
+# --------
+# | CODE |
+# --------
 
 Main
