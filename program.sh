b1520d661516e71adc1a02d8b87237f29e449a09
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-01-21 18:11:43 -0300

    Paths of the program

--- program.sh
+++ program.sh
@@ -1,3 +1,71 @@
 #!/bin/bash
 
-echo "Hello world!"
+# ---------
+# FUNCTIONS
+# ---------
+function Main() {
+  way=$( \
+    zenity --list --text "What you want?" --radiolist \
+    --column "pick" --column "options" \
+    TRUE "Fix bugs" \
+    FALSE "Programs" \
+    FALSE "Languages" \
+  );
+
+  # Checks if user clicked "Cancel"
+  if [ $? -eq 1 ];
+  then
+    exit 1
+  fi
+
+  # Redirects to the desired option
+  if [ "$way" == "Fix bugs" ];
+  then
+    Bugs
+  elif [ "$way" == "Programs" ];
+  then
+    Programs
+  else
+    Languages
+  fi
+}
+
+function Bugs() {
+  options=$( \
+    zenity --list --title "Fix bugs" --checklist \
+    --column "pick" --column "options" \
+    TRUE "Double-click with touchpad" \
+    FALSE "Input of sound not recognize" \
+    --separator=":"\
+  );
+}
+
+function Programs() {
+  options=$( \
+    zenity --list --title "Programs" --text "Select the programs you prefer" --checklist \
+    --column "pick" --column "options" \
+    FALSE "Git" \
+    FALSE "PHP" \
+    FALSE "Python" \
+    FALSE "R" \
+    --separator=":"\
+  );
+}
+
+function Languages() {
+  options=$( \
+    zenity --list --title "Languages" --text "Select the languages you prefer" --checklist \
+    --column "pick" --column "options" \
+    FALSE "Git" \
+    FALSE "PHP" \
+    FALSE "Python" \
+    FALSE "R" \
+    --separator=":"\
+  );
+}
+
+# ---------
+# CODE
+# ---------
+
+Main
