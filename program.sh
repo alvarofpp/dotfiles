40b0226cc6c6a9861c24c800b3aa53a662eb8bc9
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-01-22 11:51:34 -0300

    Installing Spotify

--- program.sh
+++ program.sh
@@ -74,17 +74,32 @@ function Programs() {
     --separator=":"\
   );
 
-  if [[ -z "$options" ]]; then
-    echo "Nothing"
-  else
-    echo "Something"
-  fi
+  IFS=":"
+  for opt in $options; do
+    case $opt in
+      "Git") Git ;;
+      "Spotify") Spotify ;;
+    esac
+  done
+  IFS=""
 }
 
 function Git() {
   echo "Installing Git"
   sudo apt-get install git
   echo "(Git) Successfully installed"
+}
+
+function Spotify() {
+  echo "Adding the Spotify repository signing keys to be able to verify downloaded packages"
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+  echo "Adding the Spotify repository"
+  echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+  echo "Updating list of available packages"
+  sudo apt-get update
+  echo "Installing Spotify"
+  sudo apt-get install spotify-client
+  echo "(Spotify) Successfully installed"
 }
 
 
