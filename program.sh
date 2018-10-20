b270d3d2a749986e3793cc70075f5530fba83e47
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-10-19 21:33:04 -0300

    Spotify key

--- program.sh
+++ program.sh
@@ -132,7 +132,7 @@ function SoundControl() {
 
 function Spotify() {
   printTerminal green "Adding the Spotify repository signing keys to be able to verify downloaded packages"
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
   printTerminal green "Adding the Spotify repository"
   echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
   printTerminal green "Updating list of available packages"
