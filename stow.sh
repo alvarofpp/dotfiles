052e2c0016b454c95742177ac877445887f8a391
Álvaro Paiva <alvarofepipa@gmail.com>
2026-03-02 22:05:19 -0300

    chore: Updates stow script

--- stow.sh
+++ stow.sh
@@ -3,5 +3,6 @@
 DOTFILES_DIR="$HOME/dotfiles"
 
 stow -d "$DOTFILES_DIR" -t "$HOME" home
+stow -d "$DOTFILES_DIR" -t "$HOME" ai
 sudo "$(which stow)" -d "$DOTFILES_DIR" -t /etc etc
 sudo "$(which stow)" -d "$DOTFILES_DIR" -t /mnt/c/Users/alvar windows
